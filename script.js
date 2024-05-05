6218f29e182973b406c5584a2cad33b110820f54
Anurag_Mishra <105621998+Anuragm473@users.noreply.github.com>
2024-05-05 13:19:44 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const year=document.querySelector(".year")
-const date=new Date().getFullYear()
-year.textContent=date
-const navel=document.querySelector(".mobile--btn")
-const headerel=document.querySelector(".header")
-navel.addEventListener('click',()=>{
-    headerel.classList.toggle('nav-open')
-})
